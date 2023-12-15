3f5fcd0778f3099b65026ef495328e47c0e89735
김지훈 <jkjh1717@naver.com>
2023-12-15 14:52:28 +0900

    Add new test1.java file
    A new java file named test1 has been introduced. The file contains two static final strings for messages and two methods, namely main and goodbyeFunction. The main method prints a message and calls the goodbyeFunction, which in turn prints a series of farewell messages.

---
+++ src/test1.java
@@ -0,0 +1,18 @@
+package src;
+
+public class test1 {
+    private static final String MESSAGE = "안녕하세요";
+    private static final String GOODBYE_MESSAGE = "안녕히가세요";
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+        goodbyeFunction();
+    }
+
+    public static void goodbyeFunction() {
+        System.out.println(GOODBYE_MESSAGE);
+        System.out.println("Goodbye!");
+        System.out.println("Have a nice day!");
+        System.out.println("See you later!");
+    }
+}
