5da9556eb63c7791b96e2beb31c2f50f21db3d58
김지훈 <jkjh1717@naver.com>
2023-12-18 09:55:44 +0900

    Add English goodbye message and function in test1.java
    The update includes the addition of ENGLISH_GOODBYE_MESSAGE constant and a corresponding englishGoodbyeFunction in test1.java. This function prints out the English goodbye message to increase the application's usability for English-speaking users.

--- src/test1.java
+++ src/test1.java
@@ -3,10 +3,12 @@ package src;
 public class test1 {
     private static final String MESSAGE = "안녕하세요";
     private static final String GOODBYE_MESSAGE = "안녕히가세요";
+    private static final String ENGLISH_GOODBYE_MESSAGE = "Goodbye";
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         goodbyeFunction();
+        englishGoodbyeFunction();
     }
 
     /**
@@ -17,5 +19,30 @@ public class test1 {
         System.out.println("Goodbye!");
         System.out.println("Have a nice day!");
         System.out.println("See you later!");
+    }
+
+    /**
+     * Prints a goodbye message in English
+     */
+    public static void englishGoodbyeFunction() {
+        System.out.println(ENGLISH_GOODBYE_MESSAGE);
+    }
+}
+
+class java {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+class goodbye {
+    public static void koreanGoodByeFunction() {
+        System.out.println();
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
     }
 }
